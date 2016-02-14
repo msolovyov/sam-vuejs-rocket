79436f1a7eb837e98b805f22aa237431b5e9cb5f
Michael Solovyov <spinalfluid@gmail.com>
2016-02-14 05:53:26 -0800

    fix js style mistakes

--- rocket.js
+++ rocket.js
@@ -1,12 +1,14 @@
 
-const COUNTER_MAX = 10 ;
+/*global Vue*/
+
+var COUNTER_MAX = 10
 
 var rocket = new Vue({
   el: '#representation',
-  ready() {
+  ready () {
 
   },
-  //model
+  // model
   data: {
     model: {
       counter: COUNTER_MAX,
@@ -17,59 +19,55 @@ var rocket = new Vue({
 
   },
 
-  //state
+  // state
   computed: {
-    stateReady: function() {
-      return ((this.model.counter === COUNTER_MAX) && !this.model.started && !this.model.launched && !this.model.aborted);
+    stateReady: function () {
+      return ((this.model.counter === COUNTER_MAX) && !this.model.started && !this.model.launched && !this.model.aborted)
     },
-    stateCounting:  function() {
-      return ((this.model.counter <= COUNTER_MAX) && (this.model.counter >= 0) && this.model.started &&  !this.model.launched && !this.model.aborted) ;
-    },
-
-    stateLaunched: function() {
-      return ((this.model.counter == 0) && this.model.started && this.model.launched && !this.model.aborted) ;
+    stateCounting: function () {
+      return ((this.model.counter <= COUNTER_MAX) && (this.model.counter >= 0) && this.model.started && !this.model.launched && !this.model.aborted)
     },
 
-    stateAborted: function() {
+    stateLaunched: function () {
+      return ((this.model.counter === 0) && this.model.started && this.model.launched && !this.model.aborted)
+    },
+
+    stateAborted: function () {
       return (
-        (  this.model.counter <= COUNTER_MAX) && (this.model.counter >= 0)
-        && this.model.started && !this.model.launched && this.model.aborted ) ;
+        (this.model.counter <= COUNTER_MAX) && (this.model.counter >= 0) && this.model.started && !this.model.launched && this.model.aborted)
     }
 
-
-
   },
-  //actions
+  // actions
   methods: {
     decrement: function () {
-      setTimeout(function(){
-        rocket.model.counter = rocket.model.counter - 1;
-        rocket.nextAction();
-      }, 1000);
-
+      setTimeout(function () {
+        rocket.model.counter = rocket.model.counter - 1
+        rocket.nextAction()
+      }, 1000)
     },
-    start() {
-      rocket.model.started = true;
-      rocket.nextAction();
+    start () {
+      rocket.model.started = true
+      rocket.nextAction()
     },
-    launch(){
-      rocket.model.launched = true;
-      rocket.nextAction();
+    launch () {
+      rocket.model.launched = true
+      rocket.nextAction()
     },
-    abort(){
-      rocket.model.aborted = true;
-      rocket.nextAction();
+    abort () {
+      rocket.model.aborted = true
+      rocket.nextAction()
     },
     nextAction: function () {
-      if ( rocket.stateCounting){
-        if(rocket.model.counter > 0){
-          rocket.decrement();
+      if (rocket.stateCounting) {
+        if (rocket.model.counter > 0) {
+          rocket.decrement()
         }
         if (rocket.model.counter === 0) {
-          rocket.launch();
+          rocket.launch()
         }
       }
     }
 
   }
-});
+})
